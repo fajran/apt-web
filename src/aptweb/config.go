456c24b8ba5fff3e72f018e00c10356e7a478197
Fajran Iman Rusadi <fajran@gmail.com>
2014-11-01 10:24:02 +0100

    Add repo base url to config

--- src/aptweb/config.go
+++ src/aptweb/config.go
@@ -9,14 +9,23 @@ import (
 type Config struct {
 	AptGetPath   string `json:"apt-get"`
 	AptCachePath string `json:"apt-cache"`
-	DistDir      string `json:"dist-dir"`
-	DistList     []Dist `json:"dists"`
+
+	DistDir  string `json:"dist-dir"`
+	DistList []Dist `json:"dists"`
+
+	RepoBaseUrl string `json:"repo-base-url"`
+	RepoList    []Repo `json:"repo-list"`
 }
 
 type Dist struct {
 	Name string `json:"name"`
 	Path string `json:"path"`
 	Arch string `json:"arch"`
+}
+
+type Repo struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
 }
 
 func NewConfigFromJson(r io.Reader) (*Config, error) {
@@ -28,7 +37,7 @@ func NewConfigFromJson(r io.Reader) (*Config, error) {
 		return nil, err
 	}
 
-	if len(config.AptGetPath) == 0 || len(config.AptCachePath) == 0 || len(config.DistDir) == 0 {
+	if len(config.AptGetPath) == 0 || len(config.AptCachePath) == 0 || len(config.DistDir) == 0 || len(config.RepoBaseUrl) == 0 {
 		return nil, errors.New("Incomplete config")
 	}
 
