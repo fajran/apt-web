e731bb5dcddd774c2b6525e8bbfe99fb603b2857
Fajran Iman Rusadi <fajran@gmail.com>
2014-11-08 09:59:11 +0100

    Rename main command line to apt-web

--- src/aptweb/cmd/apt-web/main.go
+++ src/aptweb/cmd/apt-web/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"aptweb"
 	"aptweb/server"
-	"os"
 )
 
 func main() {
+	address := ":8080"
+
 	f, _ := os.Open("config.json")
 	aptWebConfig, _ := aptweb.NewConfigFromJson(f)
 	serverConfig := &server.Config{
-		Address:      ":8080",
+		Address:      address,
 		DocumentRoot: "www/",
 	}
 
 	s := server.NewServer(aptWebConfig, serverConfig)
+
+	fmt.Printf("Starting apt-web server. Listening to %s\n", address)
 	s.ListenAndServe()
 }
