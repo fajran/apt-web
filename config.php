1f854b09eae449f37831af1d2696cdd14e7d3e9d
Fajran Iman Rusadi <fajran@gmail.com>
2015-06-21 00:13:01 +0200

    Keep sample repo and mirror list empty

--- config.php
+++ config.php
@@ -8,12 +8,12 @@ $_repo_mirror_base = 'http://archive.ubuntu.com/ubuntu';
 
 // List of available repositories
 $_repo_list = array(
-		array('ubuntu-8.04-i386', 'Ubuntu 8.04 "Hardy Heron" i386'),
+	//	array('ubuntu-8.04-i386', 'Ubuntu 8.04 "Hardy Heron" i386'),
 );
 
 // Mirror list
 $_mirror_list = array(
-		array('http://kambing.ui.edu/ubuntu', 'KAMBING.ui.edu'),
+	//	array('http://kambing.ui.edu/ubuntu', 'KAMBING.ui.edu'),
 );
 
 // Location of apt-get and apt-cache files
